bdcfb9e1c5126fc8e63240f55a32c4d7df73b2e0
ZS <zsalik@gmail.com>
2019-04-18 16:55:10 -0400

    Completed snake.js

--- snake.js
+++ snake.js
@@ -39,3 +39,27 @@ const rndPos = table => ({
   x: rndPos(0)(table.cols - 1),
   y: rndPos(0)(table.rows - 1)
 });
+
+//Initial state
+const initialState = () => ({
+  cols: 20,
+  rows: 14,
+  moves: [EAST],
+  snake: [],
+  apple: { x: 16, y: 2 }
+});
+
+const next = spec({
+  rows: prop("rows"),
+  cols: prop("cols"),
+  moves: nextMoves,
+  snake: nextSnake,
+  apple: nextApple
+});
+
+const enqueue = (state, move) =>
+  validMove(move)(state)
+    ? merge(state)({ moves: state.moves.concat([move]) })
+    : state;
+
+module.exports = { EAST, NORTH, SOUTH, WEST, initialState, enqueue, next };
