0d91aab24fb6d47c36d67fa36081cb0ca92dc013
ZS <zsalik@gmail.com>
2019-04-18 17:14:02 -0400

    Added Key events code, game loop and completed script

--- cli.js
+++ cli.js
@@ -22,3 +22,43 @@ const Matrix = {
       Matrix.addCrash(state)
     )(state)
 };
+
+//Key events
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on("keypress", (str, key) => {
+  if (key.ctrl && key.name === "c") process.exit();
+  switch (key.name.toUpperCase()) {
+    case "W":
+    case "K":
+    case "UP":
+      State = Snake.enqueue(State, Snake.NORTH);
+      break;
+    case "A":
+    case "H":
+    case "LEFT":
+      State = Snake.enqueue(State, Snake.WEST);
+      break;
+    case "S":
+    case "J":
+    case "DOWN":
+      State = Snake.enqueue(State, Snake.SOUTH);
+      break;
+    case "D":
+    case "L":
+    case "RIGHT":
+      State = Snake.enqueue(State, Snake.EAST);
+      break;
+  }
+});
+
+//Game loop
+const show = () =>
+  console.log("\x1Bc" + Matrix.toString(Matrix.fromState(State)));
+const step = () => (State = Snake.next(State));
+
+//Main
+setInterval(() => {
+  step();
+  show();
+}, 80);
