78dd5fef1b49477fc75fc2052439b7d210fb8920
ZS <zsalik@gmail.com>
2019-04-18 17:07:22 -0400

    Added Matrix operations to cli.js

--- cli.js
+++ cli.js
@@ -2,3 +2,23 @@ const readline = require("readline");
 const Snake = require("./snake");
 const base = require("./base");
 Object.getOwnPropertyNames(base).map(p => (global[p] = base[p]));
+
+//Mutable state
+let State = Snake.initialState();
+
+//Matrix operations
+const Matrix = {
+  make: table => rep(rep(".")(table.cols))(table.rows),
+  set: val => pos => adjust(pos.y)(adjust(pos.x)(k(val))),
+  addSnake: state => pipe(...map(Matrix.set("X"))(state.snake)),
+  addApple: state => Matrix.set("o")(state.apple),
+  addCrash: state => (state.snake.length == 0 ? map(map(k("#"))) : id),
+  toString: xsxs => xsxs.map(xs => xs.join(" ")).join("\r\n"),
+  fromState: state =>
+    pipe(
+      Matrix.make,
+      Matrix.addSnake(state),
+      Matrix.addApple(state),
+      Matrix.addCrash(state)
+    )(state)
+};
